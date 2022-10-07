1d289781ed78540c3c5637d6d14f7c62be60c7a8
xiaosong-zhai <115197218+xiaosong-zhai@users.noreply.github.com>
2022-10-06 17:43:16 -0700

    Add files via upload

---
+++ lab3extendweightgrade.java
@@ -0,0 +1,13 @@
+package lab4.zxsipromise.lab3;
+
+public class lab3extendweightgrade {
+    private int totalpoint; //instance private variables
+    private double earnpoint;
+    private double percentage;
+    public void getgrade(int tp,double ea,double per)   //The getgrade method
+    {
+        totalpoint = tp;
+        earnpoint = ea;
+        percentage = per;
+    }
+}
