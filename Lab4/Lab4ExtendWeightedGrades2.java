446b1166cbe289adf3ca8525fbf6555389e3f72a
xiaosong-zhai <115197218+xiaosong-zhai@users.noreply.github.com>
2022-10-19 22:32:23 -0700

    Update Lab4ExtendWeightedGrades2.java

--- Lab4/Lab4ExtendWeightedGrades2.java
+++ Lab4/Lab4ExtendWeightedGrades2.java
@@ -1,58 +1,85 @@
 package lab4;
 import java.util.ArrayList;
-import java.util.Scanner;
-public class Lab4ExtendWeightedGrades2 {
+
+public class Lab4ExtendWeightedGrades {
+    
     /*
-    create 3 ArrayLists to store user's input values
-    enter the values in a row, Press SPACE to separate and ENTER to go to next step
-    use if to judge the values ,and use method that created in the first class.
-    if true to next step
-   
+    The points of an assignment must be above 0;
+    method Used to determine whether the value entered by the user is greater than 0
+    If the total point of a assignment is not greater than 0, then printout wrong
+    if true,the go to the next step
      */
-    public static void main(String[] args) {
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Enter the total points of the assignments in a row, Press SPACE to separate and ENTER to end");
-        String inputStr1 = sc1.nextLine();
-        Scanner st1 = new Scanner(inputStr1);
-        ArrayList assignList = new ArrayList();
-        while (st1.hasNextInt()) {
-            assignList.add(st1.nextInt());
-        }
-        System.out.println(assignList);
-        if (lab4.Lab4ExtendWeightedGrades.determin(assignList) == true) {
-            Scanner sc2 = new Scanner(System.in);
-            System.out.println("Enter the earned points of the assignments in a row, Press SPACE to separate and ENTER to end");
-            String inputStr2 = sc2.nextLine();
-            Scanner st2 = new Scanner(inputStr2);
-            ArrayList earnedlist = new ArrayList();
-            while (st2.hasNextDouble()) {
-                earnedlist.add(st2.nextDouble());
+    public static boolean determin(ArrayList<Integer> assignList) {
+        for (int i = 0; i < assignList.size(); i++) {
+            if (assignList.get(i) < 0) {
+                return false;
             }
-            System.out.println(earnedlist);
-            if (lab4.Lab4ExtendWeightedGrades.compare(assignList,earnedlist) == true) {
-                Scanner sc3 = new Scanner(System.in);
-                System.out.println("Enter the percentage of the assignments in a row, Press SPACE to separate and ENTER to end");
-                String inputStr3 = sc3.nextLine();
-                Scanner st3 = new Scanner(inputStr3);
-                ArrayList percentageList = new ArrayList();
-                while (st3.hasNextDouble()) {
-                    percentageList.add(st3.nextDouble());
-                }
-                System.out.println(percentageList);
+        }
+        return true;
+    }
+    /*
+    all of assignment's percentage must be equal 100%
+    If the final percentage sum is one, go to the next step
+    if not, printout wrong
+     */
+    public static void determinper (ArrayList<Integer> assignList,ArrayList<Double> precentageList){
+        if (assignList.size() == precentageList.size()) {
+            double sumper = 0;
+            for (int i = 0; i < precentageList.size(); i++) {
+                sumper += precentageList.get(i);
+            }
+            if (sumper == 1.0){
 
-                lab4.Lab4ExtendWeightedGrades.determinper(assignList,percentageList);
-
-                lab4.Lab4ExtendWeightedGrades.calculate(assignList,earnedlist,percentageList);
             }else {
-                System.out.println("The points your entered is not correct." + "\n"
-                        + "1.The numbers of these elements need match the first row" + "\n"
-                        + "2.The elements of second row need to lower or equal the first row");
+                System.out.println("The whole percentages of assignments must equal 100%");
             }
         }else {
-            System.out.println("The points of the asignment your entered is not correct(points >= 0),Please enter again");
-
+            System.out.println("The numbers of these elements do not match the first row");
+        }
+    }
+    /*
+    The student's earnedpoint cannot be greater than the total points of an assignment
+    if greater, then return false
+    if true,then go to next step.
+     */
+    public static boolean compare(ArrayList<Integer> assignList, ArrayList<Double> earnedList) {
+        if (assignList.size() == earnedList.size()) {
+//            System.out.println("The numbers of these elements do not match the first row");
+            for (int i = 0; i < assignList.size(); i++) {
+                if (earnedList.get(i) > assignList.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }else{
+            return false;
         }
     }
 
+    /*
+    method used to calculate the final grade with user's input values.
+    then based on the final grade to determinate which scope of it and printout A-F
+     */
+    public static double calculate(ArrayList<Integer> assignList, ArrayList<Double> earnedList,ArrayList<Double> precentageList) {
+        double finalGrade = 0;
+        for (int i = 0; i < assignList.size(); i++) {
+            finalGrade += earnedList.get(i) / assignList.get(i) * precentageList.get(i) * 100;
+        }
+        System.out.println("Your finalGrade is: " + finalGrade);
+        if (finalGrade >= 0.0 && finalGrade < 60.0) {
+            System.out.println("Your grade belongs to : " + 'F');
+        } else if (finalGrade >= 60.0 && finalGrade <70.0) {
+            System.out.println("Your grade belongs to : " + 'D');
+        } else if (finalGrade >= 70.0 && finalGrade <80.0) {
+            System.out.println("Your grade belongs to : " + 'C');
+        } else if (finalGrade >= 80.0 && finalGrade <80.9999) {
+            System.out.println("Your grade belongs to : " + 'B');
+        }else if (finalGrade >= 90.0 && finalGrade <=100.0){
+            System.out.println("Your grade belongs to : " + 'A');
+        }else{
+            System.out.println("Your grade is not correct");
+        }
+        return finalGrade;
+    }
 }
 
