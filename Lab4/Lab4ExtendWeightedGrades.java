f302db4ef70fba4dee98c68673d04e2a8f4da86f
xiaosong-zhai <115197218+xiaosong-zhai@users.noreply.github.com>
2022-10-21 15:59:17 -0700

    Update Lab4ExtendWeightedGrades.java

--- Lab4/Lab4ExtendWeightedGrades.java
+++ Lab4/Lab4ExtendWeightedGrades.java
@@ -2,13 +2,24 @@ package lab4;
 import java.util.ArrayList;
 
 public class Lab4ExtendWeightedGrades {
-    
+    private double finalGrade;
+
+    public double getfinalGrade(ArrayList<Integer> assignList, ArrayList<Double> earnedList,ArrayList<Double> precentageList) {
+        for (int i = 0; i < assignList.size(); i++) {
+            this.finalGrade += earnedList.get(i) / assignList.get(i) * precentageList.get(i) * 100;
+        }
+        return finalGrade;
+    }
+
+    public Lab4ExtendWeightedGrades(){
+        }
+
     /*
-    The points of an assignment must be above 0;
-    method Used to determine whether the value entered by the user is greater than 0
-    If the total point of a assignment is not greater than 0, then printout wrong
-    if true,the go to the next step
-     */
+        The points of an assignment must be above 0;
+        method Used to determine whether the value entered by the user is greater than 0
+        If the total point of a assignment is not greater than 0, then printout wrong
+        if true,the go to the next step
+         */
     public static boolean determin(ArrayList<Integer> assignList) {
         for (int i = 0; i < assignList.size(); i++) {
             if (assignList.get(i) < 0) {
@@ -60,25 +71,20 @@ public class Lab4ExtendWeightedGrades {
     method used to calculate the final grade with user's input values.
     then based on the final grade to determinate which scope of it and printout A-F
      */
-    public static double calculate(ArrayList<Integer> assignList, ArrayList<Double> earnedList,ArrayList<Double> precentageList) {
-        double finalGrade = 0;
-        for (int i = 0; i < assignList.size(); i++) {
-            finalGrade += earnedList.get(i) / assignList.get(i) * precentageList.get(i) * 100;
-        }
+    public static void calculate(double finalGrade) {
         System.out.println("Your finalGrade is: " + finalGrade);
         if (finalGrade >= 0.0 && finalGrade < 60.0) {
             System.out.println("Your grade belongs to : " + 'F');
-        } else if (finalGrade >= 60.0 && finalGrade <70.0) {
+        } else if (finalGrade >= 60.0 && finalGrade < 70.0) {
             System.out.println("Your grade belongs to : " + 'D');
-        } else if (finalGrade >= 70.0 && finalGrade <80.0) {
+        } else if (finalGrade >= 70.0 && finalGrade < 80.0) {
             System.out.println("Your grade belongs to : " + 'C');
-        } else if (finalGrade >= 80.0 && finalGrade <89.9999) {
+        } else if (finalGrade >= 80.0 && finalGrade < 89.9999) {
             System.out.println("Your grade belongs to : " + 'B');
-        }else if (finalGrade >= 90.0 && finalGrade <=100.0){
+        } else if (finalGrade >= 90.0 && finalGrade <= 100.0) {
             System.out.println("Your grade belongs to : " + 'A');
-        }else{
+        } else {
             System.out.println("Your grade is not correct");
         }
-        return finalGrade;
     }
 }
